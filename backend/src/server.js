ab14e87da3555e8a39a9bc22ba07300dae20e92a
Luiz Felipe Pereira de Lima <luizfelipe5007@gmail.com>
2020-04-24 14:00:54 -0300

    Add Eslint and Prettier

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-//filtra quem vai acessar o backend
+// filtra quem vai acessar o backend
 app.use(cors())
 
-//diz ao express que receberei requisicoes em json
+// diz ao express que receberei requisicoes em json
 app.use(express.json())
 
-app.post('/cadastro', (req, res) => {
+app.get('/cadastro', (req, res) => {
 })
 
 app.listen(3333, () => {
-    console.log('Server On');
-    console.log('http://localhost:3333');
+  console.log('Server On')
+  console.log('http://localhost:3333')
 })
