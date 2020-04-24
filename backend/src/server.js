7cf1e6f26ce0e4133f8cb25f81a7375785eac660
Luiz Felipe Pereira de Lima <luizfelipe5007@gmail.com>
2020-04-24 11:59:32 -0300

    Start Backend

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+//filtra quem vai acessar o backend
+app.use(cors())
+
+//diz ao express que receberei requisicoes em json
+app.use(express.json())
+
+app.post('/cadastro', (req, res) => {
+})
+
+app.listen(3333, () => {
+    console.log('Server On');
+    console.log('http://localhost:3333');
+})
