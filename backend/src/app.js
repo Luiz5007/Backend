07f2eca38d8d1bd92e226f328f1de39e2326ef81
Luiz Felipe Pereira de Lima <luizfelipe5007@gmail.com>
2020-04-27 11:16:37 -0300

    Ajustes e correção do DB

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const routes = require('./app/routes/routes')
+const cors = require('cors')
+
+// filtra quem vai acessar o backend
+app.use(cors())
+
+// diz ao express que receberei requisicoes em json
+app.use(express.json())
+
+app.use(routes)
+
+module.exports = app
