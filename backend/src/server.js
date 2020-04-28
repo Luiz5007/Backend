122c8b1fab5350c76ddda7aa4b73050f215717bf
Luiz Felipe Pereira de Lima <luizfelipe5007@gmail.com>
2020-04-28 14:59:06 -0300

    Server.js ajustado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
-const app = require("./app");
-const port = 3333;
+const app = require('./app')
+const port = 3333
 
-require("./infra/database");
+require('./infra/database')
 
 app.listen(port, async () => {
-  console.log("Server On");
-  console.log("http://localhost:3333");
-});
+  console.log('Server On')
+  console.log('http://localhost:3333')
+})
