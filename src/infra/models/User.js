c146d085f0518bc7c12166b29d108703e264c72d
Luiz Felipe Pereira de Lima <luizfelipe5007@gmail.com>
2020-05-04 15:38:02 -0300

    "Arrumando o Eslint e Prettier"
    This reverts commit eb0649aa6b9fb0247944fefbcb5f4b0570e576de.

--- src/infra/models/User.js
+++ src/infra/models/User.js
@@ -1,11 +1,14 @@
 const { Model, DataTypes } = require('sequelize')
 
 class User extends Model {
-  static init (sequelize) {
-    super.init({
-      email: DataTypes.STRING,
-      password: DataTypes.STRING
-    }, { sequelize })
+  static init(sequelize) {
+    super.init(
+      {
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+      },
+      { sequelize },
+    )
   }
 }
 
